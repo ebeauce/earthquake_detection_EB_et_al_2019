30e8c87da9745f0dba08ec465f187299ccde31d8
ebeauce <ebeauce@mit.edu>
2019-05-14 17:25:03 -0400

    New dropbox link.

---
+++ scripts/00_download_data.py
@@ -0,0 +1,19 @@
+import os
+import zipfile
+
+link = 'https://www.dropbox.com/sh/w288vojhm5f9hy8/AADP1aXKAea4VhfvDitSLCk3a?dl=0'
+
+print('Download the data to {}'.format(os.getcwd()))
+
+os.system('wget -c {}'.format(link))
+
+#print('Check your current working directory, and extract the content of the downloaded folder here.')
+
+filename = link[link.rfind('/')+1:]
+
+zip_ = zipfile.ZipFile('./{}'.format(filename), mode='r')
+
+print('Extract the data in the current working direcotry: {}'.format(os.getcwd()))
+
+zip_.extractall('.')
+zip_.close()
